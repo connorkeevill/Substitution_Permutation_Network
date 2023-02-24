e515a58ec5fe0f931e633d1b7704de7008d65e58
Connor Keevill <connor.keevill.19@gmail.com>
2023-02-24 13:56:36 +0000

    Implement constructor

--- SubstitutionPermutationNetwork.cpp
+++ SubstitutionPermutationNetwork.cpp
@@ -1,1 +1,11 @@
 #include "SubstitutionPermutationNetwork.h"
+
+template<int blockSize>
+SubstitutionPermutationNetwork<blockSize>::SubstitutionPermutationNetwork(bitset<blockSize> key,
+																		  SBox sBox,
+																		  PBox<blockSize> pBox)
+{
+	this->key = key;
+	this->sBox = sBox;
+	this->pBox = pBox;
+}
