9e730cf0ea7edb468636d30af3fb6834566bcc30
Connor Keevill <connor.keevill.19@gmail.com>
2023-02-25 11:54:50 +0000

    Add some explanatory comments

--- SubstitutionPermutationNetwork.cpp
+++ SubstitutionPermutationNetwork.cpp
@@ -1,10 +1,19 @@
 #include "SubstitutionPermutationNetwork.h"
 #include "math.h"
 
-template<int blockSize, int keySize>
+/**
+ * Constructor.
+ *
+ * @tparam blockSize the network's block size.
+ * @tparam keySize the networks key size.
+ * @param key the network's key.
+ * @param sBox the s-box to use for substitutions.
+ * @param pBox the permutation table to use for permuting.
+ */
+ template<int blockSize, int keySize>
 SubstitutionPermutationNetwork<blockSize, keySize>::SubstitutionPermutationNetwork(bitset<keySize> key,
-																		  SBox sBox,
-																		  PBox<blockSize> pBox)
+																				   SBox sBox,
+																				   PBox<blockSize> pBox)
 {
 
 	this->key = key;
@@ -12,12 +21,23 @@ SubstitutionPermutationNetwork<blockSize, keySize>::SubstitutionPermutationNetwo
 	this->pBox = pBox;
 }
 
+/**
+ * Returns the ciphertext which is a result of encrypting the given plaintext ith this network's key.
+ *
+ * @tparam blockSize the block size for this network.
+ * @tparam keySize the key size for this network.
+ * @param plaintext the plaintext to encrypt.
+ * @return the ciphertext.
+ */
 template<int blockSize, int keySize>
 bitset<blockSize> SubstitutionPermutationNetwork<blockSize, keySize>::encrypt(bitset<blockSize> plaintext)
 {
+	// Each round key will move through the key by four bits for each round. This expression gets the number of rounds
+	// which will happen under this key schedule.
 	int rounds = (key.count() - blockSize) / 4;
 
-	for(int round = 0; round < rounds; round++)
+	// Perform the iterations over the network.
+	for(int round = 0; round < rounds - 1; round++)
 	{
 		bitset<blockSize> roundKey = getRoundKey(round);
 		whiten(plaintext, roundKey);
@@ -31,6 +51,13 @@ bitset<blockSize> SubstitutionPermutationNetwork<blockSize, keySize>::encrypt(bi
 	return plaintext;
 }
 
+/**
+ * Returns the round key for the given round.
+ * Each round key shifts through the encryption key by 4 bits.
+ *
+ * @param round the round of encrytption
+ * @return
+ */
 template<int blockSize, int keySize>
 bitset<blockSize> SubstitutionPermutationNetwork<blockSize, keySize>::getRoundKey(int round)
 {
@@ -44,6 +71,12 @@ bitset<blockSize> SubstitutionPermutationNetwork<blockSize, keySize>::getRoundKe
 	return roundKey;
 }
 
+/**
+ * Perform key whitening by xor-ing.
+ *
+ * @param state the state to whiten.
+ * @param roundkey the round key to use for whitening.
+ */
 template<int blockSize, int keySize>
 void SubstitutionPermutationNetwork<blockSize, keySize>::whiten(bitset<blockSize> &state, bitset<blockSize> &roundkey)
 {
